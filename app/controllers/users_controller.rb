c120332e6907d41faca5d8bd23a9f4e295c73fed
GGi1 <learn@learn12-2.localdomain>
2018-05-04 16:31:40 -0700

    corrected username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,6 @@ class UsersController < ApplicationController
   def register
     @user = User.new(full_name: params[:full_name], address: params[:street_address],city: params[:city], state: params[:state], zip: params[:zip], country: params[:country], email: params[:email], user_name: params[:user_name], password: params[:pw])
 
-
-
     cookies[:user_name]=params[:user_name]
     if User.exists?(user_name: @user.user_name)
       flash[:notice] = "User already exists!!!"
